fa0de2c3cac757ce3bc9491731f84e96f6cd42f2
Lauryn-Owens <laurynowens83@gmail.com>
2024-02-22 23:13:36 -0800

    accidently removed import ReactDOM from react-dom; from modal.jsx

--- src/Components/RandomCocktail/modal.jsx
+++ src/Components/RandomCocktail/modal.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
 import style from "../../ComponentStyles/RandomCocktailStyle/modalStyle.module.css";
 
 const Modal = ({ modalOpen, closeModal }) => {
