6f6537cdc43489e018f0a7dcc793304f7161c43b
Lauryn-Owens <laurynowens83@gmail.com>
2024-02-27 21:49:04 -0800

    addeda a display message after resetting password

--- src/Pages/forgotPassword.jsx
+++ src/Pages/forgotPassword.jsx
@@ -1,7 +1,10 @@
+import{useState} from "react";
 import {Link} from 'react-router-dom';
 import style from "../PagesStyle/forgotPasswordStyle.module.css";
 
 const ForgotPassword = () => {
+    const[showMessage, setShowMessage] = useState(false);
+
     return (
           <main className={style.forgotPassword}>
                 <h1 className={style.header}>Forgot Password?</h1>
@@ -10,11 +13,18 @@ const ForgotPassword = () => {
                     <label htmlFor="email" className={style.emailLabel}>
                        Email 
                     </label>    
-                    <input className={style.emailInput} type="email" placeholder="Enter your email" />
+                    <input  required className={style.emailInput} type="email" placeholder="Enter your email" />
                     <input 
                     style={{width:'max-content', minHeight:'3rem', padding:'0.5rem 1rem'}}
-                     className={style.emailSubmit} type="submit" value="Reset Password" id="resetPassword" name="resetPassword"/>
+                     className={style.emailSubmit} type="submit" value="Reset Password" id="resetPassword" name="resetPassword"
+                     onClick={() => {
+                        setShowMessage(true);
+                     }}
+                     />
                 </form>
+                {
+                    showMessage ? <p>A email was sent!!!</p> : null
+                }
                 <Link
                 to="/login"
                 className={style.forgotPasswordLink}
